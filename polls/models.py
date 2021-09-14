e0139c0d02703d7566177bfd568e3fc74cd1766a
田邊雄大 <yts2019hc2047@gmail.com>
2021-09-14 22:19:47 +0900

    5途中。Djangoテストクライアントから

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
@@ -23,3 +24,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    
